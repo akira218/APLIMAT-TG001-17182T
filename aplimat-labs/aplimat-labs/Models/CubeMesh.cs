a21f2c7f389ea56e36e04b3ffb424259fc6d18d6
Reymond <brionesreymond0825@gmail.com>
2018-02-03 14:17:22 +0800

    Quiz#1: STARRY NIGHT

---
+++ aplimat-labs/aplimat-labs/Models/CubeMesh.cs
@@ -0,0 +1,80 @@
+﻿using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplimat_labs.Models
+{
+    public class CubeMesh
+    {
+        public Vector3 Position;
+
+        public CubeMesh()
+        {
+            this.Position = new Vector3();
+        }
+
+        public CubeMesh(Vector3 initPos)
+        {
+            this.Position = initPos;
+
+        }
+
+        public CubeMesh(float x, float y, float z)
+        {
+            this.Position = new Vector3(x, y, z);
+        }
+
+        public void Draw(OpenGL gl)
+        {
+            gl.Begin(OpenGL.GL_TRIANGLE_STRIP);
+            //front face
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y + 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y - 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y + 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y - 0.5f, this.Position.z + 0.5f);
+
+            //Right Face
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y + 0.5f, this.Position.z - 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y - 0.5f, this.Position.z - 0.5f);
+         
+
+            //Back Face
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y + 0.5f, this.Position.z - 0.5f);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y - 0.5f, this.Position.z - 0.5f);
+            
+
+            //Left Face
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y + 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y - 0.5f, this.Position.z + 0.5f);
+            gl.End();
+
+
+            //Top face
+            gl.Begin(OpenGL.GL_TRIANGLE_STRIP);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y + 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y + 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y + 0.5f, this.Position.z - 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y + 0.5f, this.Position.z - 0.5f);
+            gl.End();
+
+            //front face
+            gl.Begin(OpenGL.GL_TRIANGLE_STRIP);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y - 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y - 0.5f, this.Position.z + 0.5f);
+            gl.Vertex(this.Position.x - 0.5f, this.Position.y - 0.5f, this.Position.z - 0.5f);
+            gl.Vertex(this.Position.x + 0.5f, this.Position.y - 0.5f, this.Position.z - 0.5f);
+            gl.End();
+
+
+        }
+
+        internal void Add(CubeMesh myCube)
+        {
+            throw new NotImplementedException();
+        
+        }
+    }
+}
