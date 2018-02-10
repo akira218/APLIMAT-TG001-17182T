998a02fec878257eb895bf285ca3f56d78eb5c35
Reymond <brionesreymond0825@gmail.com>
2018-02-10 12:28:31 +0800

    Seatwork : Bouncing Cubes - Briones, Reymond Joy P. - 11367989

--- aplimat-labs/aplimat-labs/MainWindow.xaml.cs
+++ aplimat-labs/aplimat-labs/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//Seatwork - Bouncing Cubes - Briones, Reymond Joy P. - 11367989
 namespace aplimat_labs
 {
     public partial class MainWindow : Window
